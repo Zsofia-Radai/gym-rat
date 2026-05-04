aec6e72868c59ba0ca743821c61c854fddbdb9c1
Zsofia-Radai <zsofiaradai@gmail.com>
2026-05-04 12:06:56 +0200

    fix label for sessions page

--- src/layout/AppLayout.js
+++ src/layout/AppLayout.js
@@ -1,13 +1,20 @@
 import { NavLink, Outlet } from "react-router-dom";
 import layout from "./AppLayout.module.css";
 import logo from "../resources/gym-rat-icon.png";
+import { useLocation } from "react-router-dom";
 
 function AppLayout() {
+  const location = useLocation();
+
+  const isSessionsPage = location.pathname.startsWith("/workout/sessions");
+
   return (
     <div className={`${layout.page} ${layout.medium}`}>
       <header className={layout.topbar}>
         <div className={layout.titleBlock}>
-          <p className={layout.overline}>Today</p>
+          <p className={layout.overline}>
+            {isSessionsPage ? "History" : "Today"}
+          </p>
           <h2 className={layout.title}>GymRat</h2>
         </div>
         <img className={layout.logo} src={logo} alt="GymRat logo"></img>
